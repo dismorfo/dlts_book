b91bba403510f8d5a36c27882cccbd28f7b313c3
Laura Henze <laurahenze@gmail.com>
2015-03-03 14:25:58 -0500

    Adding a wrapper div and styling for metadata dropdowns

--- templates/ds-1col--node-dlts-book-metadata.tpl.php
+++ templates/ds-1col--node-dlts-book-metadata.tpl.php
@@ -6,10 +6,14 @@
  */
 ?>
 <<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?> clearfix" dir="<?php print $lang_dir ?>" data-dir="<?php print $lang_dir ?>" data-lang="<?php print $lang_language ?>" >
+  <?php if (isset($lang_options) || isset($select_multivolbook) ) : ?>
+<div class="metapane-dropdowns">
   <?php if (isset($lang_options)) : ?><div class="lang-options"><?php print locale('Available languages', NULL, $lang_language) ?>: <?php print render($lang_options) ; ?></div><?php endif; ?>
     <?php if (isset($select_multivolbook)) : ?>
    			<?php print $select_multivolbook; ?>
    	 <?php endif; ?>
+ </div>
+<?php endif; ?>
   	<?php print $ds_content; ?>
  
  
